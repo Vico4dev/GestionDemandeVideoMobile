5d3c3db57762b90edca97b72deb90131ab1ac06a
Vico4dev <114238982+Vico4dev@users.noreply.github.com>
2024-04-10 00:39:45 +0200

    s

--- resources/views/demandes/index.blade.php
+++ resources/views/demandes/index.blade.php
@@ -1,34 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Liste des Demandes</h2>
-<a href="{{ route('demandes.create') }}" class="btn btn-primary">Ajouter une demande</a>
-<table class="table">
-    <thead>
-        <tr>
-            <th>Date Demande</th>
-            <th>Demandeur</th>
-            <th>Service</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($demandes as $demande)
-        <tr>
-            <td>{{ $demande->date_demande }}</td>
-            <td>{{ $demande->demandeur_nom }} {{ $demande->demandeur_prenom }}</td>
-            <td>{{ $demande->service }}</td>
-            <td>
-                <a href="{{ route('demandes.show', $demande->id) }}" class="btn btn-success">Voir</a>
-                <a href="{{ route('demandes.edit', $demande->id) }}" class="btn btn-warning">Modifier</a>
-                <form action="{{ route('demandes.destroy', $demande->id) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Supprimer</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+    <h2>Liste des Demandes</h2>
+    <a href="{{ route('demandes.create') }}" class="btn btn-primary">Ajouter une demande</a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Date Demande</th>
+                <th>Demandeur</th>
+                <th>Service</th>
+                <th>Localisation Exacte</th>
+                <th>Commune</th>
+                <th>Validateur</th>
+                <th>Date Validation</th>
+                <th>Demande Prestataire</th>
+                <th>Date Mise en Place</th>
+                <th>Commentaires</th>
+                <th>Photo</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($demandes as $demande)
+                <tr>
+                    <td>{{ $demande->date_demande }}</td>
+                    <td>{{ $demande->demandeur_nom }} {{ $demande->demandeur_prenom }}</td>
+                    <td>{{ $demande->service }}</td>
+                    <td>{{ $demande->localisation_exacte }}</td>
+                    <td>{{ $demande->commune }}</td>
+                    <td>{{ $demande->validateur }}</td>
+                    <td>{{ $demande->date_validation }}</td>
+                    <td>{{ $demande->demande_prestataire }}</td>
+                    <td>{{ $demande->date_mise_en_place }}</td>
+                    <td>{{ $demande->commentaires }}</td>
+                    <td>{{ $demande->photo }}</td>
+                    <td>
+                        <a href="{{ route('demandes.show', $demande->id) }}" class="btn btn-success">Voir</a>
+                        <a href="{{ route('demandes.edit', $demande->id) }}" class="btn btn-warning">Modifier</a>
+                        <form action="{{ route('demandes.destroy', $demande->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Supprimer</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Controller\Demande;
+use App\Http\Controllers\DemandeController; // Modification ici
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,7 +20,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('demandes', DemandeController::class);
+Route::resource('demandes', DemandeController::class); // Modification ici
