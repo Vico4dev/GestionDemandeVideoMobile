93e58ba2caf61606e86763d16d2a5c6a42d2516e
Vico4dev <114238982+Vico4dev@users.noreply.github.com>
2024-04-10 16:27:00 +0200

    hklhkl
    h

--- resources/views/demandes/pdf.blade.php
+++ resources/views/demandes/pdf.blade.php
@@ -10,5 +10,9 @@
     <p><strong>Date de Demande:</strong> {{ $demande->date_demande->format('d/m/Y') }}</p>
     <p><strong>Nom du demandeur:</strong> {{ $demande->demandeur_nom }} {{ $demande->demandeur_prenom }}</p>
     <!-- Continuez avec le reste des détails de la demande -->
+    
+    @if($demande->photo)
+        <p><img src="{{ public_path('storage/' . $demande->photo) }}" alt="Photo de la demande" style="max-width: 100%; height: auto;"></p>
+    @endif
 </body>
 </html>
